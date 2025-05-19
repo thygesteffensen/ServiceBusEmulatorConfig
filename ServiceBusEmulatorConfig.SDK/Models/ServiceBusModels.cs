7c5f43324de93599f9372bf74a7706eb29dfdd4d
Thyge Skødt Steffensen <31892312+thygesteffensen@users.noreply.github.com>
2025-05-19 07:50:24 +0200

    refactor: Refactor to records

--- ServiceBusEmulatorConfig.SDK/Models/ServiceBusModels.cs
+++ ServiceBusEmulatorConfig.SDK/Models/ServiceBusModels.cs
@@ -1,41 +1,36 @@
-using System.Collections.ObjectModel;
 using ServiceBusEmulatorConfig.Core.Models.Emulator;
 
 namespace ServiceBusEmulatorConfig.SDK.Models
 {
-    public class ServiceBusNamespace
-    {
-        public string Name { get; set; } = string.Empty;
-        public string ConnectionString { get; set; } = string.Empty;
-        public List<ServiceBusQueue> Queues { get; set; } = new List<ServiceBusQueue>();
-        public List<ServiceBusTopic> Topics { get; set; } = new List<ServiceBusTopic>();
-    }
+    public record ServiceBusNamespace(
+        string Name,
+        string ConnectionString,
+        List<ServiceBusQueue> Queues,
+        List<ServiceBusTopic> Topics
+    );
 
-    public class ServiceBusQueue
-    {
-        public string Name { get; set; } = string.Empty;
-        public QueueProperties Properties { get; set; } = new QueueProperties();
-        public int MessageCount { get; set; }
-    }
+    public record ServiceBusQueue(
+        string Name,
+        QueueProperties Properties,
+        int MessageCount
+    );
 
-    public class ServiceBusTopic
-    {
-        public string Name { get; set; } = string.Empty;
-        public TopicProperties Properties { get; set; } = new TopicProperties();
-        public List<ServiceBusSubscription> Subscriptions { get; set; } = new List<ServiceBusSubscription>();
-    }
 
-    public class ServiceBusSubscription
-    {
-        public string Name { get; set; } = string.Empty;
-        public SubscriptionProperties Properties { get; set; } = new SubscriptionProperties();
-        public List<ServiceBusRule> Rules { get; set; } = new List<ServiceBusRule>();
-        public int MessageCount { get; set; }
-    }
+    public record ServiceBusTopic(
+        string Name,
+        TopicProperties Properties,
+        List<ServiceBusSubscription> Subscriptions
+    );
 
-    public class ServiceBusRule
-    {
-        public string Name { get; set; } = string.Empty;
-        public RuleProperties Properties { get; set; } = new RuleProperties();
-    }
+    public record ServiceBusSubscription(
+        string Name,
+        SubscriptionProperties Properties,
+        List<ServiceBusRule> Rules,
+        int MessageCount
+    );
+
+    public record ServiceBusRule(
+        string Name,
+        RuleProperties Properties
+    );
 }
